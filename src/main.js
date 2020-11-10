c22d213351d72388e310d87ac6d48e46e250c6ea
ljj <13431967860@163.com>
2020-11-10 10:49:22 +0800

    图片

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // 1.使用commonjs的模块化规范
-const {add, mul} = require('./js/mathUtils.js')
+const {add, mul} = require('./js/mathUtils.js');
 
 console.log(add(20, 30));
 console.log(mul(20, 30));
@@ -10,3 +10,11 @@ import {name, age, height} from "./js/info";
 console.log(name);
 console.log(age);
 console.log(height);
+
+// 3.依赖css文件
+require('./css/normal.css');
+
+// 4.依赖less文件
+require('./css/special.less');
+
+document.writeln('<h2>卢本伟，牛逼</h2>');
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,40 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: "dist/"
   },
   module: {
     rules: [
-      { test: /\\.css$/, use: 'css-loader' },
-      { test: /\\.ts$/, use: 'ts-loader' }
+      {
+        test: /\.css$/,
+        //css loader只负责加载
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 当加载的图片，小于limit时，会将图片编译成base64字符串形式
+              // 当加载的图片，大于limit时，需要使用file-loader模块进行加载
+              limit: 15000,
+              name: 'img/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
     ]
   }
 }
